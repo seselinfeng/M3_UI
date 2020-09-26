788b77ad1f1eabf2bcbd89f5fefe7e9a48930fcf
apple <hujie>
2020-09-26 17:51:14 +0800

    添加时租 整夜 demo

--- pages/main.py
+++ pages/main.py
@@ -5,9 +5,25 @@ from pages.hotellist import HotelList
 class Main(BasePage):
     """首页"""
 
-    def goto_hotel_list(self):
+    def goto_day_hotel_list(self):
         """
         跳转到酒店列表页
+        :return:
+        """
+        self.step('../data/main.yaml')
+        return HotelList(self._mini)
+
+    def goto_hour_hotel_list(self):
+        """
+        选择时租房后，跳转到酒店列表页
+        :return:
+        """
+        self.step('../data/main.yaml')
+        return HotelList(self._mini)
+
+    def goto_night_hotel_list(self):
+        """
+        选择时租房后，跳转到酒店列表页
         :return:
         """
         self.step('../data/main.yaml')
--- testcases/test_demo.py
+++ testcases/test_demo.py
@@ -13,11 +13,43 @@ class TestDemo:
         self.routing = self.app.start().routing()
         self.order_list = self.app.start().order_list()
 
-    @allure.title("预定流程")
+    @allure.title("日租房预定流程")
     @pytest.mark.parametrize(("name", "phone", "room_type"), get_data('test_order', '../data/test_order.yaml'))
-    def test_order(self, name, phone):
+    def test_day_order(self, name, phone):
         with allure.step("日租房下单流程"):
             self.main.goto_hotel_list().goto_hotel_detail().goto_hotel_scheduled().save_order(name, phone)
+        time.sleep(10)
+        # 去订单列表页
+        with allure.step("跳转到订单列表页面"):
+            self.routing.goto_order_list()
+        time.sleep(10)
+        with allure.step("获取当前页面第一个订单,并点击"):
+            self.order_list.get_order_list()[0].click()
+        time.sleep(10)
+        with allure.step("获取订单详情页面数据，并断言"):
+            pass
+
+    @allure.title("时租房预定流程")
+    @pytest.mark.parametrize(("name", "phone", "room_type"), get_data('test_order', '../data/test_order.yaml'))
+    def test_hour_order(self, name, phone):
+        with allure.step("时租房下单流程"):
+            self.main.goto_hour_hotel_list().goto_hotel_detail().goto_hotel_scheduled().save_order(name, phone)
+        time.sleep(10)
+        # 去订单列表页
+        with allure.step("跳转到订单列表页面"):
+            self.routing.goto_order_list()
+        time.sleep(10)
+        with allure.step("获取当前页面第一个订单,并点击"):
+            self.order_list.get_order_list()[0].click()
+        time.sleep(10)
+        with allure.step("获取订单详情页面数据，并断言"):
+            pass
+
+    @allure.title("整夜房预定流程")
+    @pytest.mark.parametrize(("name", "phone", "room_type"), get_data('test_order', '../data/test_order.yaml'))
+    def test_night_order(self, name, phone):
+        with allure.step("整夜房下单流程"):
+            self.main.goto_hour_hotel_list().goto_hotel_detail().goto_hotel_scheduled().save_order(name, phone)
         time.sleep(10)
         # 去订单列表页
         with allure.step("跳转到订单列表页面"):
